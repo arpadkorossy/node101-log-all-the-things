a0f88615ac166425add5d5164849df1575299ebc
Arpad Korossy <korossy.arpad@gmail.com>
2024-02-26 15:11:11 -0500

    Logging works, but doesn't pass tests

--- server/app.js
+++ server/app.js
@@ -1,20 +1,50 @@
-const express = require('express');
-const fs = require('fs');
-const app = express();
+var express = require('express');
+var fs = require('fs');
+var morgan = require('morgan');
+var path = require('path')
 
-app.use((req, res, next) => {
+var app = express();
+
 // write your logging code here
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.csv'), { flags: 'a' });
 
-});
+app.use(morgan(':user-agent|:date[iso]|:method|:url|:http-version|:status', { stream: accessLogStream }))
 
-app.get('/', (req, res) => {
 // write your code to respond "ok" here
-
+app.get('/', (req, res) => {
+    res.status(200).send('ok');
 });
 
 app.get('/logs', (req, res) => {
 // write your code to return a json object containing the log data here
+    // converts CSV to JSON
 
+    // Convert the data to String and split it in an array
+    let csv = fs.readFileSync('./server/log.csv');
+    const array = csv.toString().split("\n");
+    let result = [];
+    // The array[0] contains all the header columns so we store them in headers array
+    let headers = array[0].split("|");
+    // Since headers are separated, we need to traverse remaining n-1 rows.
+    for (let i = 1; i < array.length - 1; i++) {
+        let obj = {}    
+        // Split the string using pipe delimiter | and store the values in a properties array
+        let properties = array[i].split("|")
+        console.log(`array[${i}] = ${array[i]}`)
+        for (let j in headers) {
+            obj[headers[j]] = properties[j];
+            console.log(`obj[${headers[j]}] = ${obj[headers[j]]}`);
+            //console.log(`obj is ${obj}`)
+    }
+        // Add the generated object to our result array
+        result.push(obj);
+        //console.log(`result is ${result}`)
+    }
+
+    let json = JSON.stringify(result);
+    console.log(`json is ${json}`);
+    res.status(200).json(json);
+    fs.writeFileSync('output.json', json);
 });
 
 module.exports = app;
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const server = require('./app');
+const PORT = process.env.PORT || 3000;
 
-server.listen(3000, () => {
-  console.log('server is listening on 3000');
+server.listen(PORT, () => {
+  console.log(`server is listening on ${PORT}`);
 })
